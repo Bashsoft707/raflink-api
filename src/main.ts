925c05e39829037b54de5ca0bfd5452e34b09cbc
Bashsoft707 <alausabashir@gmail.com>
2025-02-22 09:24:31 +0100

    Fxi swagger docs on prod

--- src/main.ts
+++ src/main.ts
@@ -15,24 +15,36 @@ async function bootstrap() {
   const SWAGGER_ENVS = ['development', 'local'];
 
   // if (SWAGGER_ENVS.includes(ENV.NODE_DEV)) {
-    app.use(
-      ['/api'],
-      basicAuth({
-        challenge: true,
-        users: {
-          [ENV.SWAGGER_USER]: ENV.SWAGGER_PASSWORD,
-        },
-      }),
-    );
-    const config = new DocumentBuilder()
-      .setTitle('Raflink')
-      .setDescription('Raflink API documentation')
-      .setVersion('1.0')
-      .addBearerAuth()
-      .setExternalDoc('Postman Collection', '/api-json')
-      .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+  app.use(
+    ['/api'],
+    basicAuth({
+      challenge: true,
+      users: {
+        [ENV.SWAGGER_USER]: ENV.SWAGGER_PASSWORD,
+      },
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Raflink')
+    .setDescription('Raflink API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .setExternalDoc('Postman Collection', '/api-json')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
+
+  SwaggerModule.setup('api', app, document, {
+    customSiteTitle: 'Your API Documentation',
+    customfavIcon: '/favicon.ico',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  });
   // }
 
   app.useGlobalPipes(new ValidationPipe());
